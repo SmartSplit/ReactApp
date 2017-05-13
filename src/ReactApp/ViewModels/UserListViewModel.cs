43c64d71a9ade703c0f1e372007208ad52553cf5
Szymon Żak <szymekzak@o2.pl>
2017-05-13 16:28:31 +0200

    View Models

---
+++ src/ReactApp/ViewModels/UserListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactApp.DTO
+{
+    public class UserViewModel
+    {
+        public List<UserDTO> Users { get; set; }
+
+        public UserViewModel(List<UserDTO> users)
+        {
+            this.Users = users;
+        }
+    }
+}
